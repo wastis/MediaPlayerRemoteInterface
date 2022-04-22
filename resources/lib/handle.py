835ca4626d03331dfbcd02e5c8274da7ab46c653
wastis <wasti_s@t-online.de>
2022-04-22 08:38:49 +0200

    v1.0.0

--- resources/lib/handle.py
+++ resources/lib/handle.py
@@ -4,7 +4,7 @@
 #
 #	https://github.com/wastis/MediaPlayerRemoteInterface
 #
-#	PulseEqualizerGui is free software; you can redistribute it and/or modify
+#	Media Player Remote Interface is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU Lesser General Public License as published
 #	by the Free Software Foundation; either version 3 of the License,
 #	or (at your option) any later version.
